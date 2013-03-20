3c0e0ba4dfa925c6ef26dabbf45199a6f3713e84
Matt Wise <matt@nextdoor.com>
2013-03-20 14:36:13 -0700

    Closes #10: Implements custom throttling rules for ZK API calls
    The ZookeeperClient API throttling code is moved entirely into the
    shims.ZookeeperClient class. This allows us to leverage class-level
    variables that define the number of calls to throttle and the average
    time between them.
    
    We now expose a set_rate_limiter() method in the ZookeeperClient that
    the KazooServiceRegistry class uses to define the rate limit settings.
    These settings are considered global for the entire ZookeeperClient
    object now as well -- gets, sets, etc are all included.
    
    Setting the rate_limit settings to None, False or 0 will disable them.
    
    Change-Id: I783cffa56bcf35e3d799ccaf63eb32c1b17ab19b

--- nd_service_registry/shims.py
+++ nd_service_registry/shims.py
@@ -19,33 +19,131 @@ Copyright 2012 Nextdoor Inc."""
 __author__ = 'matt@nextdoor.com (Matt Wise)'
 
 import logging
-from kazoo.client import KazooClient
-from nd_service_registry.funcs import rate_limiter
+import time
 
-# Our default variables
-from version import __version__ as VERSION
+from functools import wraps
+from kazoo.client import KazooClient
+
+# Get a default logger
+log = logging.getLogger(__name__)
 
 
 class ZookeeperClient(KazooClient):
     """Shim-layer that provides some safety controls"""
+    def __init__(self, *args, **kwargs):
+        """Initialize the rate limiter code, then handle the default init"""
 
-    @rate_limiter(targetAvgTimeBetweenCalls=1, numCallsToAverage=50)
+        # Iniitialize our rate limiter
+        self.set_rate_limiter()
+
+        # Finish object initialization
+        super(ZookeeperClient, self).__init__(*args, **kwargs)
+
+    def set_rate_limiter(self, time=0, calls=0):
+        """Set rate limiting settings.
+
+        args:
+            time: Number of seconds to average between ZK requests (int)
+            calls: Number of calls to determine averages over (int)
+        """
+
+        # Make sure that if the values are None or False, reset them
+        if not isinstance(time, int):
+            time=0
+        if not isinstance(calls, int):
+            calls=0
+
+        log.debug('Set up rate limiting. Max %s avg between last %s calls.' %
+                  (calls, time))
+        self.previous_calls = []
+        self.num_calls_to_average_over = calls
+        self.target_avg_between_calls = time
+
+    def rate_limiter(func):
+        """Provides a decorator for rate-limiting function.
+
+        The time between decorated function calls will be averaged over the
+        last self.num_calls_to_average_over calls. A dynamically generated
+        'sleep' time will be applied to new calls if the overall rate is
+        higher than configured.
+
+        The rate-limiting is blocking. Your code will not complete until
+        the sleep timer is finished.
+        """
+
+        @wraps(func)
+        def _rate_limited_function(self, *args, **kwargs):
+            # Start out assuming we will not need to throttle
+            throttle = False
+
+            # Have there even been enough calls to trigger the timing check?
+            if ((self.num_calls_to_average_over > 0) and
+               (len(self.previous_calls) > self.num_calls_to_average_over)):
+                # Get the time elapsed between now and the last
+                # 'self.num_calls_to_average_over' calls. eg. If
+                # self.num_calls_to_average_over is 10, check how
+                # much time has elapsed between this call, and the
+                # call 10-calls ago.
+                elapsed = time.time() - \
+                    self.previous_calls[-self.num_calls_to_average_over]
+
+                # Now get a rough average amount of time between the calls
+                avg_between_calls = elapsed / self.num_calls_to_average_over
+                log.debug('[%s] Avg time between last %s calls: %s, target: %s'
+                          % (func.__name__, self.num_calls_to_average_over,
+                             avg_between_calls, self.target_avg_between_calls))
+
+                # If the average time between each call is less than the
+                # self.target_avg_between_calls then we trigger throttling.
+                if avg_between_calls < self.target_avg_between_calls:
+                    log.warning('[%s] Too little time between last %s calls '
+                                'for func. Throttling.' % (func.__name__,
+                                self.num_calls_to_average_over))
+                    throttle = True
+
+            # If we're going to throttle, determine how long to wait
+            if throttle:
+                sleep = self.target_avg_between_calls - avg_between_calls
+                log.debug('[%s] Sleeping: %s' % (func.__name__, sleep))
+                time.sleep(sleep)
+
+            # Now go ahead and run our function
+            log.debug('[%s] Calling with args[%s] and kwargs[%s]' %
+                      (func.__name__, args, kwargs))
+
+            ret = func(self, *args, **kwargs)
+
+            # Record that we ran the function
+            self.previous_calls.append(time.time())
+            log.debug('[%s] Last %s calls: %s' %
+                      (func.__name__, self.num_calls_to_average_over,
+                       self.previous_calls[-self.num_calls_to_average_over:]))
+
+            # Keep only the last num_calls_to_average_over+1 in the
+            # previous_calls list so that we keep its size reasonable.
+            calls_to_keep = self.num_calls_to_average_over + 1
+            self.previous_calls = self.previous_calls[-calls_to_keep:]
+
+            return ret
+        return _rate_limited_function
+
+    @rate_limiter
     def retry(self, *args, **kwargs):
         return super(ZookeeperClient, self).retry(*args, **kwargs)
 
-    @rate_limiter(targetAvgTimeBetweenCalls=1, numCallsToAverage=50)
+    @rate_limiter
     def get(self, *args, **kwargs):
         return super(ZookeeperClient, self).get(*args, **kwargs)
 
-    @rate_limiter(targetAvgTimeBetweenCalls=10, numCallsToAverage=50)
+    @rate_limiter
     def set(self, *args, **kwargs):
         return super(ZookeeperClient, self).set(*args, **kwargs)
 
-    @rate_limiter(targetAvgTimeBetweenCalls=10, numCallsToAverage=50)
+    @rate_limiter
     def create(self, *args, **kwargs):
         return super(ZookeeperClient, self).create(*args, **kwargs)
 
-    @rate_limiter(targetAvgTimeBetweenCalls=10, numCallsToAverage=50)
+    @rate_limiter
     def delete(self, *args, **kwargs):
         return super(ZookeeperClient, self).delete(*args, **kwargs)
 
