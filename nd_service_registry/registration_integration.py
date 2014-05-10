30d9cb27771f5ee87c6f6c6c3dab5ef86edce938
Matt Wise <matt@nextdoor.com>
2014-05-10 12:03:30 -0700

    Initial basic EphemeralNode integration tests

--- nd_service_registry/registration_integration.py
+++ nd_service_registry/registration_integration.py
@@ -33,8 +33,7 @@ class RegistrationBaseTests(KazooTestHarness):
     def tearDown(self):
         self.teardown_zookeeper()
 
-    def test_initial_object_setup(self):
-        """Ensure that the RegistrationBase object is instantiated properly"""
+    def test_init(self):
         nd = KazooServiceRegistry(server=self.server)
         path = '%s/unittest' % self.sandbox
         data = {'unittest': 'data'}
@@ -57,7 +56,6 @@ class RegistrationBaseTests(KazooTestHarness):
         self.assertEquals(None, reg1._watcher.get()['stat'])
 
     def test_public_methods(self):
-        """Ensure that setting data/state work properly"""
         nd = KazooServiceRegistry(server=self.server)
         zk = nd._zk
         path = '%s/unittest' % self.sandbox
@@ -115,4 +113,64 @@ class RegistrationBaseTests(KazooTestHarness):
         waituntil(reg1.get, current_stat, 5)
         self.assertEquals(None, reg1.get()['stat'])
         self.assertFalse(reg1.state())
+        self.assertEquals(None, zk.exists(path))
+
+
+class EphemeralNodeTests(KazooTestHarness):
+
+    # A flag for filtering nose tests
+    integration = True
+
+    def setUp(self):
+        self.setup_zookeeper()
+        self.server = 'localhost:20000'
+        self.sandbox = "/tests/ephemeral-%s" % uuid.uuid4().hex
+
+    def tearDown(self):
+        self.teardown_zookeeper()
+
+    def test_init_and_behavior(self):
+        nd = KazooServiceRegistry(server=self.server)
+        zk = nd._zk
+        path = '%s/unittest' % self.sandbox
+        data = {'unittest': 'data'}
+        eph1 = EphemeralNode(zk=nd._zk, path=path, data=data)
+        waituntil(eph1.data, None, 5)
+
+        # The EphemeralNode object DOES immediately register itself in
+        # zookeeper, so we should be able to pull that data from Zookeeper
+        # right away.
+        (data, stat) = zk.get(path)
+        self.assertNotEquals(None, stat)
+        self.assertTrue('"unittest":"data"' in data)
+
+        # Now, lets intentionally change the data in ZOokeeper directly,
+        # the EphemeralNode should immediately re-set the data.
+        current_stat = eph1.get()
+        zk.set(path, value='bogus')
+        waituntil(eph1._watcher.get, current_stat, 5)
+        (data, stat) = zk.get(path)
+        self.assertTrue('"unittest":"data"' in data)
+        self.assertFalse('bogus' in data)
+
+        # Now lets intentionally delete the node and see it get re-run
+        current_stat = eph1.get()
+        zk.delete(path)
+        waituntil(eph1._watcher.get, current_stat, 5)
+        (data, stat) = zk.get(path)
+        self.assertTrue('"unittest":"data"' in data)
+        self.assertTrue(zk.exists(path))
+
+        # Try disabling the node. If the node gets recreated automatically in
+        # some way (by some rogue daemon), then we should destroy it.
+        def path_exists_in_zk():
+            if zk.exists(path):
+                return True
+
+            return False
+
+        eph1.stop()
+        self.assertEquals(None, zk.exists(path))
+        zk.create(path)
+        waituntil(path_exists_in_zk, True, 5)
         self.assertEquals(None, zk.exists(path))
