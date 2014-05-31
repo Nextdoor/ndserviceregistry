d9dbb1ee43cb94a89377c5c006779fbc709966d5
Matt Wise <matt@nextdoor.com>
2014-05-30 19:47:07 -0700

    Issue #38: Monitor non-existent paths for children changes.
    This patch deals with issue #38. If you create a Watcher() on a path
    that doesn't exist yet, Kazoo cannot create a ChildrenWatch object on
    that path. In the past, we logged this event and moved on ... which
    meant that when the path was created later on (and children were added),
    your app would not be notified.
    
    Now we register a second DataWatch on that non-existent path and look
    for the state of the path to change. When it changes, we check if a
    previous ChildrenWatch had been created. If not, we create a new one and
    then record that. If one had already been created, we quickly exit the
    function.
    
    I've provided full integration tests for this behavior, but I have not
    yet created standard Unit tests. I will do unit tests in a separate
    checkin.

---
+++ nd_service_registry/watcher_integration.py
@@ -0,0 +1,157 @@
+import uuid
+import mock
+import time
+
+from kazoo.testing import KazooTestHarness
+from nd_service_registry import KazooServiceRegistry
+from nd_service_registry.watcher import Watcher
+
+
+def waituntil(predicate, predicate_value, timeout, period=0.1, mode=1):
+    mustend = time.time() + timeout
+    while time.time() < mustend:
+        if mode == 1:
+            comparison = predicate() != predicate_value
+        else:
+            comparison = predicate() == predicate_value
+
+        if comparison:
+            print "Exiting timer, %s changed..." % predicate
+            return True
+        print "Sleeping, waiting for %s to change..." % predicate
+        time.sleep(period)
+    raise Exception('Failed waiting for %s to change...' % predicate)
+
+
+class WatcherIntegrationTests(KazooTestHarness):
+
+    # A flag for filtering nose tests
+    integration = True
+
+    def setUp(self):
+        self.setup_zookeeper()
+        self.server = 'localhost:20000'
+        self.sandbox = "/tests/watcher-%s" % uuid.uuid4().hex
+        nd = KazooServiceRegistry(server=self.server,
+                                  rate_limit_calls=0,
+                                  rate_limit_time=0)
+        self.zk = nd._zk
+
+    def tearDown(self):
+        self.teardown_zookeeper()
+
+    def test_watch_on_existing_path(self):
+        path = '%s/existing-path-%s' % (self.sandbox, uuid.uuid4().hex)
+        self.zk.create(path, makepath=True)
+        watch = Watcher(zk=self.zk, path=path)
+
+        # We expect there to be valid data...
+        self.assertEquals(None, watch.get()['data'])
+        self.assertNotEquals(None, watch.get()['stat'])
+        self.assertEquals({}, watch.get()['children'])
+
+        # Now register a child node, and expect the data to change
+        child_path = '%s/my_child_test' % path
+        self.zk.create(child_path)
+
+        # Before progressing, wait for Zookeeper to have kicked off
+        # all of its notifications to the client.
+        def get_children():
+            print "got: %s" % watch.get()['children']
+            return watch.get()['children']
+        waituntil(get_children, {}, timeout=5)
+
+        # Now expect the watch to be kicked off
+        self.assertTrue('my_child_test' in watch.get()['children'])
+
+    def test_watch_on_nonexistent_path_with_data_change(self):
+        path = '%s/nonexistent-path-%s' % (self.sandbox, uuid.uuid4().hex)
+        watch = Watcher(zk=self.zk, path=path)
+
+        # Initially, we expect the data, children count and stat data
+        # to be None because the node doesn't exist.
+        self.assertEquals(None, watch.get()['data'])
+        self.assertEquals(None, watch.get()['stat'])
+        self.assertEquals({}, watch.get()['children'])
+
+        # Now, lets go and set the data path with something. We expect the
+        # watcher now to actually register the change. Use the waituntil()
+        # method to wait until the data has been updated.
+        def get_data():
+            return watch.get()['data']
+
+        self.zk.create(path, value="{'foo': 'bar'}", makepath=True)
+        waituntil(get_data, None, timeout=5, mode=1)
+
+        # Now verify that the data has been updated in our Watcher
+        self.assertEquals(
+            {'string_value': "{'foo': 'bar'}"}, watch.get()['data'])
+        self.assertNotEquals(None, watch.get()['stat'])
+        self.assertEquals({}, watch.get()['children'])
+
+    def test_watch_on_nonexistent_path_with_children_change(self):
+        path = '%s/nonexistent-path-%s' % (self.sandbox, uuid.uuid4().hex)
+        watch = Watcher(zk=self.zk, path=path)
+
+        # Initially, we expect the data, children count and stat data
+        # to be None because the node doesn't exist.
+        self.assertEquals(None, watch.get()['data'])
+        self.assertEquals(None, watch.get()['stat'])
+        self.assertEquals({}, watch.get()['children'])
+
+        # Now register a child node under the path, and expect the
+        # data to change
+        child_path = '%s/my_child_test' % path
+        self.zk.create(child_path, makepath=True)
+
+        # Wait until the children list changes
+        def get_children():
+            return watch.get()['children']
+        waituntil(get_children, {}, timeout=5)
+
+        # Now verify that the data has been updated in our Watcher
+        self.assertTrue('my_child_test' in watch.get()['children'])
+
+    def test_watch_on_nonexistent_path_with_multiple_data_changes(self):
+        path = '%s/nonexistent-path-%s' % (self.sandbox, uuid.uuid4().hex)
+        watch = Watcher(zk=self.zk, path=path)
+
+        # In order to track the number of callbacks that are registered,
+        # we register a mocked method in the callbacks.
+        callback_checker = mock.Mock()
+        callback_checker.test.return_value = True
+        watch.add_callback(callback_checker.test)
+
+        # Initially, we expect the data, children count and stat data
+        # to be None because the node doesn't exist.
+        self.assertEquals(None, watch.get()['data'])
+        self.assertEquals(None, watch.get()['stat'])
+        self.assertEquals({}, watch.get()['children'])
+
+        # Now register a child node under the path, and expect the
+        # data to change
+        child_path = '%s/my_child_test' % path
+        self.zk.create(child_path, makepath=True)
+
+        # Wait until the children list changes
+        def get_children():
+            return watch.get()['children']
+        waituntil(get_children, {}, timeout=5)
+
+        # Now verify that the data has been updated in our Watcher
+        self.assertTrue('my_child_test' in watch.get()['children'])
+
+        # Now make a series of data changes and wait for each one to
+        # take effect before moving on to the next one.
+        def get_data():
+            return watch.get()['data']
+        for i in xrange(1, 5):
+            self.zk.set(path, value='%s' % i)
+            waituntil(get_data, {'string_value': '%s' % i}, timeout=5, mode=2)
+
+        # The right number of calls is 7. There are a few upfront calls when
+        # the Watcher object is initialized, and after that there are a few
+        # calls for the updated node-data when new children are added. The
+        # call count would be MUCH higher (11++) if we were recursively
+        # creating Watches though.
+        self.assertEquals(7, len(callback_checker.test.mock_calls))
