1159f928e5e69b5742dcba52f72c0dab2eee4e75
Matt Wise <matt@nextdoor.com>
2014-08-12 08:32:20 -0700

    Add Kazoo 2.0 support as well as tests.
    Kazoo 2.0 modified the KazooClient.add_auth() method to actually receive
    and get the data for the asynchronous add authentication call. This
    deadlocks when its wrapped inside of a KazooClient.retry() method.
    
    The fix is to call add_auth_async() directly and ignore the result.
    Additionally, I've thrown in tests that triggered this behavior, and
    added Kazoo 2.0 to the testing matrix for Travis.

--- nd_service_registry/sr_integration.py
+++ nd_service_registry/sr_integration.py
@@ -1,4 +1,5 @@
 import mock
+import uuid
 
 from kazoo.testing import KazooTestHarness
 from nd_service_registry import KazooServiceRegistry
@@ -37,3 +38,36 @@ class KazooServiceRegistryIntegrationTests(KazooTestHarness):
 
         self.assertTrue(mock.call(True) in callback_checker.test.mock_calls)
         self.assertTrue(mock.call(False) in callback_checker.test.mock_calls)
+
+
+class KazooServiceRegistryIntegrationTestsWithAuth(KazooTestHarness):
+    # A flag for filtering nose tests
+    integration = True
+
+    def setUp(self):
+        self.setup_zookeeper()
+        self.sandbox = "/tests/sr-%s" % uuid.uuid4().hex
+        self.server = 'localhost:20000'
+        self.username = 'user'
+        self.password = 'pass'
+        self.ndsr = KazooServiceRegistry(server=self.server,
+                                         username=self.username,
+                                         password=self.password,
+                                         rate_limit_calls=0,
+                                         rate_limit_time=0)
+        self._zk = self.ndsr._zk
+
+    def tearDown(self):
+        self.teardown_zookeeper()
+        self.ndsr._initialized = False
+
+    def test_set_node_with_acl(self):
+        path = '%s/set_node_test_1' % self.sandbox
+        self.ndsr.set_node(path, {})
+        acls, znode_stat = self.ndsr._zk.get_acls(path)
+
+        # ACLs are returned back in a slightly different format than
+        # when we set them, so we have to dig into the return value
+        # a bit for tests.
+        self.assertEquals(self.ndsr._acl[0], acls[0])
+        self.assertEquals(self.ndsr._acl[1], acls[1])
